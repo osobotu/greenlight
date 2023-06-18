abe96a30754a34948494b9bfcc2c51b0fd53608c
Stephen Obotu Oduh <osobotu@gmail.com>
2023-06-18 22:21:19 +0100

    added functionality to parse json, handle bad requests, validate json requests

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,10 +6,38 @@ import (
 	"time"
 
 	"github.com/osobotu/greenlight/internal/data"
+	"github.com/osobotu/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +58,32 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) getAllMoviesHandler(w http.ResponseWriter, r *http.Request) {
+	movies := []data.Movie{
+		{
+			ID:        1,
+			CreatedAt: time.Now(),
+			Title:     "Casablanca",
+			Runtime:   102,
+			Genres:    []string{"drama", "romance", "war"},
+			Version:   1,
+		},
+		{
+			ID:        2,
+			CreatedAt: time.Now(),
+			Title:     "Ted Lasso",
+			Runtime:   102,
+			Genres:    []string{"drama", "romance", "sports"},
+			Version:   1,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
